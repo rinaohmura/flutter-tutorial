e9b6f79a3d4d687a46f013df7677a2eba8a1c200
tanakarn98 <56716847+rinaohmura@users.noreply.github.com>
2022-05-02 15:48:48 +0900

    screen_login

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,126 @@
 import 'package:flutter/material.dart';
-    
-    void main() => runApp(MyApp()); /*1*/
-    
-    class MyApp extends StatelessWidget {/*2*/
-      @override
-      Widget build(BuildContext context) { /*3*/
-        return MaterialApp(  /*4*/
-          title: 'Welcome to Flutter',
-          home: Scaffold( /*5*/
-            appBar: AppBar( /*6*/
-              title: Text('Welcome to Flutter'),
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var _size = MediaQuery.of(context).size;
+    final ButtonStyle raisedButtonStyle = ElevatedButton.styleFrom(
+      onPrimary: Colors.white,
+      primary: Color.fromRGBO( 22, 175, 175,1),
+      elevation:0,
+      fixedSize: Size(
+          _size.width * 0.9, 
+          44,
+        ),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+    );
+    final ButtonStyle raisedButtonStyle2 = ElevatedButton.styleFrom(
+      onPrimary: Color.fromRGBO( 22, 175, 175,1),
+      primary: Colors.white,
+      elevation:0,
+      fixedSize: Size(
+          _size.width * 0.9, 
+          44,
+        ),
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      side: BorderSide(width: 1.0, color: Color.fromRGBO( 22, 175, 175,1)),
+    );
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image(
+              image: const AssetImage('assets/images/logo.png'),
+              width: 300,
             ),
-            body: Center( /*7*/
-              child: Text('Hello World'),
+            Image(
+              image: const AssetImage('assets/images/charactor.png'),
+              width: 200,
             ),
-          ),
-        );
-      }
-    }
+            ElevatedButton(
+              onPressed: () {}, 
+              child: Text('新規登録(無料)'),
+              style: raisedButtonStyle,
+              ),
+            OutlinedButton(
+              onPressed: () {}, 
+              child: Text('ログイン'),style: raisedButtonStyle2,
+              )
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
